9600b0416aad17858f859629ffdaf557786ed975
Nando Vieira <fnando.vieira@gmail.com>
2013-01-21 22:16:17 -0200

    #fml

--- lib/pry-meta.rb
+++ lib/pry-meta.rb
@@ -1,7 +1,4 @@
 require "pry"
 require "pry-nav"
-require "pry-stack_explorer"
-require "pry-rescue"
-require "pry-exception_explorer"
-require 'pry-remote'
+require "pry-remote"
 require "awesome_print"
