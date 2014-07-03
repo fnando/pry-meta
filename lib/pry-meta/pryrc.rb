499abdfd95880604e94a29fa86aec4fc301370c7
Nando Vieira <fnando.vieira@gmail.com>
2014-07-03 14:34:56 -0300

    Add default pryrc file.

---
+++ lib/pry-meta/pryrc.rb
@@ -0,0 +1,34 @@
+Pry.config.editor = ENV.fetch('EDITOR', 'subl -w')
+
+Pry.config.prompt = proc do |obj, level, _|
+  prompt = ''
+  prompt << "#{Rails.version}@" if defined?(Rails)
+  prompt << "#{RUBY_VERSION}"
+  "#{prompt} (#{obj})> "
+end
+
+Pry.config.exception_handler = proc do |output, exception, _|
+  output.puts "\e[31m#{exception.class}: #{exception.message}"
+  output.puts "from #{exception.backtrace.first}\e[0m"
+end
+
+if defined?(Rails)
+  require 'rails/console/app'
+  require 'rails/console/helpers'
+  TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+end
+
+begin
+  require 'pry-meta'
+
+  Pry.config.print = proc do |output, value|
+    Pry::Helpers::BaseHelpers
+      .stagger_output("=> #{value.ai}", output)
+  end
+
+  Pry.commands.alias_command 'c', 'continue'
+  Pry.commands.alias_command 's', 'step'
+  Pry.commands.alias_command 'n', 'next'
+rescue LoadError => error
+  warn '=> Unable to load pry-meta'
+end
